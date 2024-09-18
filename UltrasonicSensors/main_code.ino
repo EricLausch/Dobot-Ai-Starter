8cc3cfd748bd26e3c8e43cd708bfab47ab89961b
Evan-CL <aymanezaths@gmail.com>
2024-09-18 16:32:00 +0200

    Update main_code.ino

--- UltrasonicSensors/main_code.ino
+++ UltrasonicSensors/main_code.ino
@@ -12,14 +12,14 @@ void avoidObstacles() {
     float distanceRight = AIStarter_SmartBotGetSonar(SONAR1);
     float distanceFront = AIStarter_SmartBotGetSonar(SONAR2);
 
-    if (distanceFront < DIS) {
+    if (distanceFront < DIS) { // der vordere Sensor lest ein Abstand mehr als DIS
         AIStarter_SmartBotSetMovment(BACK, 100);
         delay(1000);
-    } else if (distanceLeft < DIS) {
+    } else if (distanceLeft < DIS) { // der linke Sensor lest ein Abstand mehr als DIS
         AIStarter_SmartBotSetMovment(RIGHT, 100);
         delay(1000);
     } else if (distanceRight < DIS) {
-        AIStarter_SmartBotSetMovment(LEFT, 100);
+        AIStarter_SmartBotSetMovment(LEFT, 100); // der rechte Sensor lest ein Abstand mehr als DIS
         delay(1000);
     } else if (distanceLeft < distanceRight) {
         AIStarter_SmartBotSetMovment(RIGHT, 100);
@@ -27,7 +27,7 @@ void avoidObstacles() {
         AIStarter_SmartBotSetMovment(LEFT, 100);
     }
 
-    // Default case: move forward if no obstacles
+    // Keine Obstacles
     if (distanceFront >= DIS && distanceLeft >= DIS && distanceRight >= DIS) {
         AIStarter_SmartBotSetMovment(FRONT, 100);
     }
